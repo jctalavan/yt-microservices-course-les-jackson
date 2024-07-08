b36e54ee3a98d3a7e2e6222cf8028ee52a905d17
jctalavan <jctalavancanelo@gmail.com>
2024-07-08 13:28:51 +0200

    Remove bin & obj folders + request + createPlatform endpoint

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -10,7 +10,7 @@ namespace PlatformService.Controllers
     [Route("api/[controller]")]
     public class PlatformsController(IPlatformRepository platformRepository, IMapper mapper) : ControllerBase
     {
-        [HttpGet(nameof(GetAllPlatforms))]
+        [HttpGet(Name = nameof(GetAllPlatforms))]
         public ActionResult<IEnumerable<PlatformReadResponse>> GetAllPlatforms()
         {
             IEnumerable<Platform> platforms = platformRepository.GetAllPlatforms();
@@ -21,16 +21,40 @@ namespace PlatformService.Controllers
             return Ok(platformsReadResponse);
         }
 
-        [HttpGet(nameof(GetPlatform))]
+        [HttpGet("{id}", Name = nameof(GetPlatform))]
         public ActionResult<PlatformReadResponse> GetPlatform(int id)
         {
             Platform? platform = platformRepository.GetPlatform(id);
 
-            //El resultado podría ser NULL
+            if(platform == null)
+                return NotFound();
 
             PlatformReadResponse platformReadResponse = mapper.Map<PlatformReadResponse>(platform);
 
             return Ok(platformReadResponse);
+        }
+
+        [HttpPost(Name = nameof(CreatePlatform))]
+        public ActionResult<PlatformReadResponse> CreatePlatform([FromBody] PlatformCreateRequest platformCreateRequest)
+        {
+            if(platformCreateRequest == null)
+                return BadRequest();
+
+            Platform platform = mapper.Map<Platform>(platformCreateRequest);
+
+            platformRepository.CreatePlatform(platform);
+            if (platformRepository.SaveChanges())
+            {
+                PlatformReadResponse platformReadResponse = mapper.Map<PlatformReadResponse>(platform);
+
+                return CreatedAtRoute(
+                    routeName: nameof(GetPlatform),
+                    routeValues: new {id= platformReadResponse.Id},
+                    value: platformReadResponse
+                );
+            }
+
+            return BadRequest();
         }
     }
 }
